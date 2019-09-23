bbaf75045b60d0432f9f6b8037b359b86337d148
Domantas Kiaušinis <domantaskiausinis@gmail.com>
2019-09-23 20:14:10 +0300

    Project setup

---
+++ TankzMultiplayer/TankzClient/TankzWindow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TankzClient
+{
+    public partial class TankzClient : Form
+    {
+        public TankzClient()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TankzMultiplayer/TankzServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace TankzServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server is running...");
+            //TCP UDP stuff here
+        }
+    }
+}
