7c6bdba72cd879818985861084f3ea024847c5f0
Rokas Tverijonas <36890057+rokastve@users.noreply.github.com>
2019-10-10 15:00:28 +0300

    Network Start
    - NetworkManager on client side
    - Server packet reader

---
+++ TankzMultiplayer/TankzClient/Framework/NetworkManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TankzClient.Framework
+{
+    public class NetworkManager
+    {
+        string serverIP = "localhost";
+        int port = 420;
+
+        #region Singleton
+
+        private static NetworkManager instance = null;
+        public static NetworkManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new NetworkManager();
+                return instance;
+            }
+        }
+        #endregion
+        public void SendPacket(string text)
+        {
+            TcpClient client = new TcpClient(serverIP, port);
+            int bytecount = Encoding.ASCII.GetByteCount(text);
+
+            byte[] sendData = new byte[bytecount];
+
+            sendData = Encoding.ASCII.GetBytes(text);
+
+            NetworkStream stream = client.GetStream();
+
+            stream.Write(sendData, 0, sendData.Length);
+
+            stream.Close();
+            client.Close();
+        }
+    }
+}
--- TankzMultiplayer/TankzServer/Program.cs
+++ TankzMultiplayer/TankzServer/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace TankzServer
 {
@@ -7,8 +9,31 @@ namespace TankzServer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Server is running...");
-            //TCP UDP stuff here
+            IPAddress ip = Dns.GetHostEntry("localhost").AddressList[0];
+            TcpListener server = new TcpListener(ip, 420);
+            TcpClient client = default(TcpClient);
+
+            try
+            {
+                server.Start();
+                Console.WriteLine("Server is running...");
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.Read();
+            }
+
+            while (true)
+            {
+                client = server.AcceptTcpClient();
+                byte[] receivedbuffer = new byte[100];
+                NetworkStream stream = client.GetStream();
+
+                stream.Read(receivedbuffer, 0, receivedbuffer.Length);
+
+                string msg = Encoding.ASCII.GetString(receivedbuffer, 0, receivedbuffer.Length);
+                Console.WriteLine(msg);
+            }
         }
     }
 }
