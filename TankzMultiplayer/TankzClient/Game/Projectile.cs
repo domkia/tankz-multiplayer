514ff6d1b6d31bce1368f706cd642c7c824b081f
jussla1 <justinas.slabada@ktu.edu>
2019-10-17 15:24:02 +0300

    projectile class
    simple class

---
+++ TankzMultiplayer/TankzClient/Game/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TankzClient.Framework;
+
+namespace TankzClient.Game
+{
+    class Projectile : Sprite
+    {
+        const string projectileSpritePath = "../../res/projectile_1.png";
+        int projectileRadius;
+        int explosionRadius;
+        int speed;
+        public Projectile() : base(Image.FromFile(projectileSpritePath), new Vector2(200, 200), new Vector2(20,20))
+        {
+            
+        }
+
+        public override void Update(float deltaTime)
+        {
+            base.Update(deltaTime);
+        }
+    }
+}
