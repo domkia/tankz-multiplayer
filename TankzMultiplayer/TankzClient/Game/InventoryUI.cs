abf27fb21060c3693b79985f2efe91a20ed901b1
jussla1 <justinas.slabada@ktu.edu>
2019-10-21 12:31:14 +0300

    Added new phase
    Weapon selection phase

---
+++ TankzMultiplayer/TankzClient/Game/InventoryUI.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TankzClient.Framework;
+
+namespace TankzClient.Game
+{
+    class InventoryUI : UIElement
+    {
+        private bool isOpen = false;
+        public bool IsOpen => isOpen;
+        public InventoryUI(Rectangle rect) : base(rect)
+        {
+
+        }
+        public override void Click(Point mousePos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenPanel()
+        {
+            isOpen = true;
+        }
+
+        public void ClosePanel()
+        {
+            isOpen = false;
+        }
+    }
+}
---
+++ TankzMultiplayer/TankzClient/Game/TankWeaponSelection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TankzClient.Framework;
+
+namespace TankzClient.Game
+{
+    class TankWeaponSelection : TankPhase
+    {
+        InventoryUI inventoryUI;
+        public TankWeaponSelection(Tank tank) : base(tank)
+        {
+
+        }
+        public override void Update(float deltaTime)
+        {
+            if (Input.IsKeyDown(System.Windows.Forms.Keys.W))
+            {
+                if (inventoryUI.IsOpen) inventoryUI.ClosePanel();
+                else
+                {
+                    inventoryUI.OpenPanel();
+                }
+            }
+        }
+    }
+}
