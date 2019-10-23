df82e429d5ce898708cc4d56a8a659d8777f7497
Domantas Kiaušinis <domantaskiausinis@gmail.com>
2019-10-24 00:10:57 +0300

    Fully customizable scorch terrain

---
+++ TankzMultiplayer/TankzClient/Game/TerrainScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using TankzClient.Framework;
+
+namespace TankzClient.Game
+{
+    class TerrainScene : Scene
+    {
+        private Terrain terrain;
+
+        public override void Load()
+        {
+            terrain = new Terrain();
+            CreateEntity(terrain);
+        }
+
+        public override void Render(Graphics context)
+        {
+            context.Clear(Color.SkyBlue);
+            base.Render(context);
+        }
+    }
+}
