ea1d3278e4b4d4f3a9526e19694f4cb5c2cc1411
Rokas Tverijonas <36890057+rokastve@users.noreply.github.com>
2019-10-29 01:11:05 +0200

    Fix
    - Added missing stuff
    - Fixed sln (had wrong project added from other folder)

--- TankzMultiplayer/TankzSignalRServer/Hubs/TestHub.cs
+++ TankzMultiplayer/TankzSignalRServer/Hubs/TestHub.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
-using TankzSignalRServer.Controllers;
 using TankzSignalRServer.Data;
 using TankzSignalRServer.Models;
 using Newtonsoft.Json;
@@ -13,15 +12,47 @@ namespace TankzSignalRServer.Hubs
     public class TestHub : Hub
     {
         private readonly TankzContext _context;
+        private static HashSet<Player> LobbyPlayers = new HashSet<Player>();
+
         public TestHub(TankzContext context)
         {
            _context = context;
         }
-        static HashSet<string> CurrentConnections = new HashSet<string>();
+        
         public override bool Equals(object obj)
         {
             return base.Equals(obj);
         }
+        [HubMethodName("ChangeReadyState")]
+        public Task ChangeState()
+        {
+            var existingPerson2 = LobbyPlayers.SingleOrDefault(p => p.ConnectionId == Context.ConnectionId);
+            existingPerson2.ReadyState = !existingPerson2.ReadyState;
+            bool allTrue = LobbyPlayers.All(i => i.ReadyState == true);
+            if (allTrue)
+            {
+                gameStart();
+            }
+            return ConnectedPeople();
+        }
+        public Task gameStart()
+        {
+            return Clients.All.SendAsync("GameStart","");
+        }
+        /// <summary>
+        /// Set name for player when joining from lobby
+        /// </summary>
+        /// <param name="name">Name</param>
+        /// <returns>Gražina pakeistą žaidėjų sąrašą</returns>
+        [HubMethodName("SetName")]
+        public Task SetName(string name)
+        {
+            var existingPerson2 = LobbyPlayers.SingleOrDefault(p => p.ConnectionId == Context.ConnectionId);
+            existingPerson2.Name = name;
+            return ConnectedPeople();
+        }
+
+        //Testing method (not used in game)
         [HubMethodName("SendMessage")]
         public Task Message()
         {
@@ -29,27 +60,34 @@ namespace TankzSignalRServer.Hubs
             string json = JsonConvert.SerializeObject(players);
             return Clients.All.SendAsync("ReceiveMessage", json);
         }
+        //Gets connected people and returns to all clients that are listening
         [HubMethodName("GetConnected")]
         public Task ConnectedPeople()
         {
-            string json = JsonConvert.SerializeObject(CurrentConnections.ToList());
-            return Clients.All.SendAsync("Players", json);
+            string json = JsonConvert.SerializeObject(LobbyPlayers.ToList());
+            return Clients.All.SendAsync("Players", json);  
         }
         public override int GetHashCode()
         {
             return base.GetHashCode();
         }
 
+        //On client connect task
+        //Add connection to list and calls for connectedPeople method
         public override Task OnConnectedAsync()
         {
-            CurrentConnections.Add(Context.ConnectionId);
+            Player player = new Player { ConnectionId = Context.ConnectionId, Name = "", Icon = "", ReadyState = false};
+            LobbyPlayers.Add(player);
             ConnectedPeople();
             return base.OnConnectedAsync();
         }
 
+        //On disconnected task
+        //Removes player form list and calls for connectedPeople method
         public override Task OnDisconnectedAsync(Exception exception)
         {
-            CurrentConnections.Remove(Context.ConnectionId);
+            var existingPerson2 = LobbyPlayers.SingleOrDefault(p => p.ConnectionId == Context.ConnectionId);
+            LobbyPlayers.Remove(existingPerson2);
             ConnectedPeople();
             return base.OnDisconnectedAsync(exception);
         }
--- TankzMultiplayer/TankzSignalRServer/Models/Player.cs
+++ TankzMultiplayer/TankzSignalRServer/Models/Player.cs
@@ -8,7 +8,9 @@ namespace TankzSignalRServer.Models
     public class Player
     {
         public int ID { get; set; }
+        public string ConnectionId { get; set; }
         public string Name { get; set; }
         public string Icon { get; set; }
+        public bool ReadyState { get; set; }
     }
 }
