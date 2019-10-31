d3eaed8363c543162a9a9b5ba58938deedc7e6f5
jussla1 <justinas.slabada@ktu.edu>
2019-10-31 09:55:49 +0200

    Properties and test
    Added properties and one not working test :)

---
+++ TankzMultiplayer/TankzTests1/Stub/TestScene.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TankzClient.Framework;
+
+namespace TankzTests1.Game.Tests
+{
+    public class TestScene : Scene
+    {
+        public override void Load()
+        {
+            
+        }
+    }
+}
