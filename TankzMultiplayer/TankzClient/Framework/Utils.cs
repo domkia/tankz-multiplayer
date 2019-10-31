6fb03b898a28dc20edd9f6ac7322bbb6b9c15b67
Domantas Kiaušinis <domantaskiausinis@gmail.com>
2019-10-31 20:41:57 +0200

    Small tweaks

--- TankzMultiplayer/TankzClient/Framework/Utils.cs
+++ TankzMultiplayer/TankzClient/Framework/Utils.cs
@@ -4,7 +4,16 @@
     {
         public static float Lerp(float a, float b, float t)
         {
-            return a + (b - a) * t;
+            return a + (b - a) * Clamp(0f, 1f, t);
+        }
+
+        public static float Clamp(float min, float max, float value)
+        {
+            if (value < min)
+                return min;
+            else if (value > max)
+                return max;
+            return value;
         }
     }
 }
