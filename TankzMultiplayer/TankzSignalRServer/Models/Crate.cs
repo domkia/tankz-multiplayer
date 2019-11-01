991373326d27fde25f280b70574abe15ea92d124
jussla1 <justinas.slabada@ktu.edu>
2019-11-01 20:30:46 +0200

    Crate class created
    server side crate class created with reference to weapon

---
+++ TankzMultiplayer/TankzSignalRServer/Models/Crate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TankzSignalRServer.Models
+{
+    public class Crate
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public Weapon Weapon { get; set; }
+    }
+}
