d300ab9a7da690d510323fe7298615d2153112ea
jussla1 <justinas.slabada@ktu.edu>
2019-11-04 01:23:46 +0200

    ICloneable interface (server side)
    - Created ICloneable interface
    - Realized this interface for crate and weapon class

--- TankzMultiplayer/TankzSignalRServer/Models/Crate.cs
+++ TankzMultiplayer/TankzSignalRServer/Models/Crate.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TankzSignalRServer.Interfaces;
 using TankzSignalRServer.Utilites;
 
 namespace TankzSignalRServer.Models
 {
-    public class Crate
+    public class Crate : ICloneable<Crate>
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public int PositionX { get; set; }
         public int PositionY { get; set; }
         public Weapon Weapon { get; set; }
+
+        public Crate Clone()
+        {
+            Crate crate = new Crate {
+                ID = this.ID, 
+                Name = this.Name, 
+                PositionX = this.PositionX, 
+                PositionY = this.PositionY, 
+                Weapon = this.Weapon
+            };
+            return crate;
+        }
     }
 }
--- TankzMultiplayer/TankzSignalRServer/Models/Weapon.cs
+++ TankzMultiplayer/TankzSignalRServer/Models/Weapon.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TankzSignalRServer.Interfaces;
 
 namespace TankzSignalRServer.Models
 {
-    public class Weapon
+    public class Weapon : ICloneable<Weapon>
     {
         public int ID { get; set; }
         public string Name { get; set; }
         public float Radius { get; set; }
         public float Explosion_Radius { get; set; }
+
+        public Weapon Clone()
+        {
+            Weapon weapon = new Weapon{
+                ID = this.ID,
+                Name = this.Name,
+                Radius = this.Radius,
+                Explosion_Radius = this.Explosion_Radius};
+            return weapon;
+        }
     }
 }
