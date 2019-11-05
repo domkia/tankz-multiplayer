3f4de6f1608ff2c6c15edf73260d5042a93cc1ca
Rokas Tverijonas <36890057+rokastve@users.noreply.github.com>
2019-11-05 11:40:50 +0200

    Login screen changes
    - Added logo in login screen
    - Moved item positions
    - Added cipher class for upcoming login/register system

--- TankzMultiplayer/TankzClient/Game/LoginScene.cs
+++ TankzMultiplayer/TankzClient/Game/LoginScene.cs
@@ -13,7 +13,9 @@ namespace TankzClient.Game
     {
         private Button loginButton;
         private Button suicideButton;
-        private InputField inputField;
+        private InputField usernameField;
+        private InputField passwordField;
+        private Sprite logo;
         bool nameError = false;
         bool waiting = false;
 
@@ -25,11 +27,11 @@ namespace TankzClient.Game
         {
             if (joining == true)
                 return;
-            if (inputField.Text.ToString().Length != 0)
+            if (usernameField.Text.ToString().Length != 0)
             {
                 waiting = true;       
 
-                string name = inputField.Text.ToString();
+                string name = usernameField.Text.ToString();
                 joining = true;
                 NetworkManager.Instance.JoinLobby(name);
             }
@@ -64,14 +66,15 @@ namespace TankzClient.Game
             networkThread.IsBackground = true;
             networkThread.Start();
 
-            inputField = CreateEntity(new InputField(140, 13, 120, 20)) as InputField;
-            inputField.SetBackgroundColor(Color.Brown);
-            loginButton = CreateEntity(new Button(270, 13, 50, 20, null, "Log In")) as Button;
+            logo = CreateEntity(new Sprite(Image.FromFile("../../res/logo.png"), new Vector2(380,80), new Vector2(500,250)))as Sprite;
+            usernameField = CreateEntity(new InputField(140, 200, 120, 20)) as InputField;
+            usernameField.SetBackgroundColor(Color.Brown);
+            loginButton = CreateEntity(new Button(270, 200, 50, 20, null, "Log In")) as Button;
             loginButton.OnClickCallback += LoginButton_OnClickCallback;
             loginButton.SetTextColor(Color.White);
             loginButton.SetColor(Color.Brown);
 
-            suicideButton = CreateEntity(new Button(10, 10, 50, 50, null, "Suicide Scene")) as Button;
+            suicideButton = CreateEntity(new Button(650, 400, 50, 50, null, "Suicide Scene")) as Button;
             suicideButton.OnClickCallback += SuicideButton_OnClickCallback;
 
             icons = new Image[]
@@ -84,14 +87,14 @@ namespace TankzClient.Game
 
         public override void Render(Graphics context)
         {
-            context.Clear(Color.Bisque);
+            context.Clear(Color.FromArgb(77,120,78));
             base.Render(context);
             if (!joining)
             {
-                context.DrawString("USERNAME:", new Font(FontFamily.GenericMonospace, 16f, FontStyle.Bold), Brushes.Black, new Point(10, 10));
+                context.DrawString("USERNAME:", new Font(FontFamily.GenericMonospace, 16f, FontStyle.Bold), Brushes.Black, new Point(10, 200));
                 if (nameError)
                 {
-                    context.DrawString("Username can't be empty", new Font(FontFamily.GenericMonospace, 16f, FontStyle.Bold), Brushes.Red, new Point(10, 30));
+                    context.DrawString("Username can't be empty", new Font(FontFamily.GenericMonospace, 16f, FontStyle.Bold), Brushes.Red, new Point(10, 230));
                 }
                 if (waiting)
                 {
@@ -104,7 +107,7 @@ namespace TankzClient.Game
             }
             for (int i = 0; i < icons.Length; i++)
             {
-                context.DrawImage(icons[i], new Point(16, 100 + 36 * i));
+                context.DrawImage(icons[i], new Point(16, 250 + 36 * i));
             }
         }
     }
